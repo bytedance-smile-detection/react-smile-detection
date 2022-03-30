b8f39712dfadd83cce9e4c91539d9fc7cbdcbc43
xcm1115 <997749670@qq.com>
2022-03-31 00:31:22 +0800

    fix: change request url

--- src/constants.js
+++ src/constants.js
@@ -2,7 +2,7 @@ const WIDTH = 28;
 const HEIGHT = 28;
 
 // const BACKEND_URL = "http://localhost:8000"; // development env
-const BACKEND_URL = "http://www.xiaoqw.icu"; // production env
+const BACKEND_URL = "https://www.xiaoqw.icu"; // production env
 
 const LENET_MODEL_URL = `${BACKEND_URL}/models/lenet/model.json`;
 const FACE_MODEL_URL = `${BACKEND_URL}/models/face/model.json`;
